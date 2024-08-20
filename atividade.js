23f59e1ef13fd03f218755367aa6beaaf2b074fc
PollyOliv <pollyoliveira_@outlook.com>
2024-08-20 20:45:58 -0300

    Meu novo commit

---
+++ atividade.js
@@ -0,0 +1,22 @@
+/*function numeroprimo(num) {
+  let cont = 0;
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      cont++;
+    }
+  }
+
+  if (cont === 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(numeroprimo(7));
+*/
+
+/*function selecaoprimo(num){
+
+*/
+
